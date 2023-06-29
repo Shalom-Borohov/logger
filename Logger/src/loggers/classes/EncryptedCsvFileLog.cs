f8e923bccc3d68e9336d882ecd75294a82191961
Shalom Borohov <shalom842633@gmail.com>
2023-06-29 10:28:33 +0300

    Fix encrypted write to csv using AES algorithm

---
+++ Logger/src/loggers/classes/EncryptedCsvFileLog.cs
@@ -0,0 +1,43 @@
+﻿using Logger.src.loggers.interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Logger.src.loggers.classes
+{
+    internal class EncryptedCsvFileLog : FileLog, ILog
+    {
+        public void ClearLog() => File.Create(FilePath);
+
+        public IEnumerable<string> ReadEntries(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteEntry(LogEntry entry)
+        {
+            using var fileStream = new FileStream(FilePath, FileMode.Append);
+            var encryptionKey = CreateEncryptionKey();
+
+            using var aes = Aes.Create();
+            aes.Key = encryptionKey;
+            fileStream.Write(aes.IV, 0, aes.IV.Length);
+
+            using var cryptoStream = new CryptoStream(fileStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+            using var encryptedWriter = new StreamWriter(cryptoStream);
+
+            encryptedWriter.WriteLine($"{entry.DateAndTime},{entry}{Environment.NewLine}");
+
+        }
+
+        // TODO: Move key to env
+        private byte[] CreateEncryptionKey()
+        {
+            byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+
+            return key;
+        }
+    }
+}
--- Logger/src/main/Program.cs
+++ Logger/src/main/Program.cs
@@ -10,12 +10,14 @@ namespace Logger
         static void Main(string[] args)
         {
             string filePath = "logs.csv";
+            string encryptedFilePath = "encryptedLogs.csv";
             var dateTime = new DateTime(2023, 6, 26, 15, 29, 57);
 
             var csvFileLog = new CsvFileLog { FilePath = filePath };
+            var encryptedCsvFileLog = new EncryptedCsvFileLog { FilePath = encryptedFilePath };
 
-            csvFileLog.WriteEntry(new LogEntry { DateAndTime = DateTime.Now, Message = "Balagan Gadol", Severity = Severity.Critical });
-            csvFileLog.ReadEntries(dateTime).ToList().ForEach(Console.WriteLine);
+            encryptedCsvFileLog.WriteEntry(new LogEntry { DateAndTime = DateTime.Now, Message = "Balagan Gadol", Severity = Severity.Critical });
+            // csvFileLog.ReadEntries(dateTime).ToList().ForEach(Console.WriteLine);
         }
     }
 }
